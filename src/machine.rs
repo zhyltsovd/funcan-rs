0f7c1c0496cfabd5e59b1be6658804cb4b5bee05
zhxdmu <zhxdmu@nb-szum>
2025-01-11 09:04:13 +0300

    machine composition

--- src/machine.rs
+++ src/machine.rs
@@ -63,3 +63,45 @@ pub trait MachineWithOutput {
     /// otherwise, `None`.
     fn output(self: &Self) -> Option<Self::Output>;
 }
+
+/// Represents the composition of two finite state machines,
+/// where the output of the first machine (`M0`) serves as the input to the second machine (`M1`).
+pub struct Comp<M0, M1> {
+    /// The first state machine.
+    pub m0: M0,
+    /// The second state machine.
+    pub m1: M1,
+}
+
+/// Allows for the composition of two finite state machines `M0` and `M1`.
+/// This trait implementation is applicable when the final values of machine `M0`
+/// can be used as inputs to machine `M1`. 
+/// 
+/// A common use case is where `M0` processes and decodes some low-level input
+/// to generate higher-level inputs for machine `M1`.
+impl<X, M0, M1> MachineTrans<X> for Comp<M0, M1>
+where
+    M0: MachineTrans<X>,
+    M1: MachineTrans<<M0 as MachineTrans<X>>::FinalValue>
+{
+    /// The final value type produced by the composed machines.
+    type FinalValue = <M1 as MachineTrans<<M0 as MachineTrans<X>>::FinalValue>>::FinalValue;
+    
+    /// Processes an input `x` by passing it through machine `M0`.
+    /// If `M0` reaches a final state, its output is passed as input to machine `M1`.
+    fn transition(self: &mut Self, x: X) {
+        self.m0.transition(x);
+        match self.m0.is_final() {
+            Some(y) => {
+                self.m1.transition(y);
+            }
+            None => {}
+        }
+    }
+
+    /// Checks if the composed machines have reached a final state.
+    /// Returns the final value of machine `M1` if it has reached a final state.
+    fn is_final(self: &Self) -> Option<Self::FinalValue> {
+        self.m1.is_final()
+    }
+}
