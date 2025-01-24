72019f5c82d54027935f406b12a9341016ad58df
zhyltsovd <zhyltsovd@gmail.com>
2025-01-24 18:44:10 +0300

    interim

--- src/client.rs
+++ src/client.rs
@@ -4,6 +4,7 @@ use crate::heartbeat::*;
 use crate::sdo::machines::*;
 use crate::raw::*;
 use crate::cobid::*;
+use crate::machine::*;
 
 struct ClientInterface {
     heartbeat: HeartbeatMachine,
@@ -23,8 +24,25 @@ impl<C: CANInterface> ClientCtx<C>
         Ok(())
     }
 
-    fn handle_node_cmd<E>(self: &mut Self, cmd: NodeCmd, node: u8) -> Result<(), E> {
-        // todo
+    fn handle_node_cmd<E>(self: &mut Self, cmd: NodeCmd, node: u8, data: [u8; 8]) -> Result<(), E> {
+        
+        match cmd {
+            NodeCmd::Emergency => {},
+            NodeCmd::Time => {},
+            NodeCmd::Pdo1Tx => {},
+            NodeCmd::Pdo1Rx => {},
+            NodeCmd::Pdo2Tx => {},
+            NodeCmd::Pdo2Rx => {},
+            NodeCmd::Pdo3Tx => {},
+            NodeCmd::Pdo3Rx => {},
+            NodeCmd::Pdo4Tx => {},
+            NodeCmd::Pdo4Rx => {},
+            NodeCmd::SdoResp => {},
+            NodeCmd::SdoReq => {},
+            NodeCmd::Heartbeat => { self.interface.heartbeat.transit(data) },
+            NodeCmd::Unused => {},
+        }
+        
         Ok(())
     }
     
@@ -33,7 +51,7 @@ impl<C: CANInterface> ClientCtx<C>
 
         match fun_code {
             FunCode::Broadcast(cmd) => self.handle_broadcast(cmd),
-            FunCode::Node(cmd, node) => self.handle_node_cmd(cmd, node),
+            FunCode::Node(cmd, node) => self.handle_node_cmd(cmd, node, frame.can_data),
         }
     }
     
