5d611178463d9fdba26d5258f07bbd927a81930c
zhyltsovd <zhyltsovd@gmail.com>
2025-10-08 10:03:52 +0300

    interim

---
+++ src/interfaces.rs
@@ -0,0 +1,10 @@
+use core::time::Duration;
+
+pub trait Responder<X> {
+    fn respond(self, x: X) -> Result<(), X>;
+}
+
+pub trait ClockInstant {
+    fn now() -> Self;
+    fn duration_since(self: &Self, i: &Self) -> Duration;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 //! # funcan-rs
 //!
 
+pub mod interfaces;
+pub mod heartbeat;
 pub mod machine;
 pub mod nmt;
 pub mod raw;
