b4a9dba786b46935ee23fa15aa51a349cf0d3395
zhyltsovd <zhyltsovd@gmail.com>
2025-11-05 14:51:28 +0300

    interim

--- src/sdo/server.rs
+++ src/sdo/server.rs
@@ -13,7 +13,7 @@ impl<const N: usize, D: Dictionary> SdoServer<N, D>
 where
     D: Default,
     D::Index: From<CanIndex>,
-    D::Object: IntoBuf
+    D::Object: for<'a> TryFrom<(D::Index, &'a [u8])> + IntoBuf
 {
     pub fn new() -> Self { 
         Self {
@@ -37,7 +37,26 @@ where
                     }
 
                     ServerOutput::FinalOutput(resp, result) => {
-                        todo!()
+                        if let ServerResult::DownloadCompleted(dindex, data, n) = result {
+                            if let Ok(index) = <D as Dictionary>::Index::try_from(dindex) {
+                                if let Ok(downloaded_value) = <D as Dictionary>::Object::try_from((index, &data[0..n])) {
+                                    self.dictionary.set(downloaded_value);
+                                    todo!() 
+                                } else {
+                                    Error(SdoError::DictionaryDecodingFailure(dindex))
+                                }                
+                            } else {
+                                Error(SdoError::DictionaryUnsupportedIndex(dindex))
+                            }
+                            
+                        } else {
+                            todo!()
+                        }
+                            //assert_eq!(index, dindex);
+                            //assert_eq!(n, 2);
+                            //let  =
+                            //    u16::from_le_bytes([data[0], data[1]]);
+                            //assert_eq!(downloaded_value, value);
                     }
 
                     out => out,
